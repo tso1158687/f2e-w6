8db705fb5316becfa36558da9129321d3c9fbb0e
jason zheng <101104044@nccu.edu.tw>
2018-07-14 14:46:10 +0800

    account style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { PaymentComponent } from './payment/payment.component';
 import { CongratulationsComponent } from './congratulations/congratulations.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ButtonModule} from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +27,8 @@ import {ButtonModule} from 'primeng/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
+    ReactiveFormsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
