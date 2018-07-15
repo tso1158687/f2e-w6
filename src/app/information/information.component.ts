98367c6000a3b99e18777805bc331eafe9491088
jason zheng <101104044@nccu.edu.tw>
2018-07-16 00:58:29 +0800

    temp complete

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-information',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./information.component.scss']
 })
 export class InformationComponent implements OnInit {
-
-  constructor() { }
+  infoForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
-
+  initForm() {
+    this.infoForm = this.fb.group({
+      name: [''],
+      phone: ['', [Validators.required]],
+      birYear: ['', [Validators.pattern('[0-9]')]],
+      birMonth: ['', [Validators.pattern('[0-9]')]],
+      birDate: ['', [Validators.pattern('[0-9]')]],
+      city: ['', [Validators.required]],
+      dist: ['', [Validators.required]],
+      addressDetail: ['', [Validators.required]]
+    });
+  }
 }
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
+  paymentForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
-
+  initForm() {
+    this.paymentForm = this.fb.group({
+      number: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      bank: ['', [Validators.required]],
+      CVV: ['', [Validators.required]],
+      MM: ['', [Validators.required]],
+      DD: ['', [Validators.required]]
+      // name: [''],
+      // phone: ['', [Validators.required]],
+      // birYear: ['', [Validators.pattern('[0-9]')]],
+      // birMonth: ['', [Validators.pattern('[0-9]')]],
+      // birDate: ['', [Validators.pattern('[0-9]')]],
+      // city: ['', [Validators.required]],
+      // dist: ['', [Validators.required]],
+      // addressDetail: ['', [Validators.required]]
+    });
+  }
 }
